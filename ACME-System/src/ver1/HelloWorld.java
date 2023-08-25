632c98942dadd7228f046b24df99263b8132dfdf
Fred Taft <e13taft@gmail.com>
2023-08-25 13:45:09 -0400

    Line added.

--- ACME-System/src/ver1/HelloWorld.java
+++ ACME-System/src/ver1/HelloWorld.java
@@ -3,8 +3,8 @@ package ver1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
 		System.out.println("Hello from Eclipse");
+		System.out.println("Fred is working locally on his branch");
 
 	}
 
